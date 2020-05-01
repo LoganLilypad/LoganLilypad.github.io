95789c7721fda5e34867a96b5059ea6edd13edb8
LoganLilypad <40219242+LoganLilypad@users.noreply.github.com>
2020-05-01 17:57:16 -0400

    Update index.js

--- control/index.js
+++ control/index.js
@@ -1,49 +1,62 @@
 if ('WebSocket' in window) {
+
+    var ws;
+    var pin;
+
     function conn() {
-    
-    var ws = new WebSocket('wss://167.172.158.214:8080');
-     
-    ws.onopen = function() {
 
-        var pin;
+        ws = new WebSocket('wss://flicket.org:443');
 
-        ws.send('P;' + document.getElementById('pin').value);
+        ws.onopen = () => {
 
-        let lastMotion = 0;
-        window.addEventListener('devicemotion', (event) => {
-            if (window.ready) {
-                pin = document.getElementById('pin').value;
-                const currentTimestamp = new Date().getTime();
-                if (currentTimestamp - lastMotion > 50) {
-                    const rotation = {
-                        x: Math.round((event.accelerationIncludingGravity.x * 10) * 20) / 20,
-                        y: Math.round((event.accelerationIncludingGravity.y * 10) * 20) / 20,
-                        z: Math.round((event.accelerationIncludingGravity.z * 10) * 20) / 20,
-                    };
+            ws.send('!P;' + document.getElementById('pin').value);
+
+            let lastMotion = 0;
+            window.addEventListener('devicemotion', (event) => {
+                alert(event);
+                if (window.ready) {
+                    pin = document.getElementById('pin').value;
+                    const currentTimestamp = new Date().getTime();
+                    if (currentTimestamp - lastMotion > 50) {
+                        const rotation = {
+                            x: Math.round((event.accelerationIncludingGravity.x * 10) * 20) / 20,
+                            y: Math.round((event.accelerationIncludingGravity.y * 10) * 20) / 20,
+                            z: Math.round((event.accelerationIncludingGravity.z * 10) * 20) / 20,
+                        };
         
-                    ws.send('UR;' + pin + ';' + [...Object.values(rotation)].join(';'));
+                        ws.send('!UR;' + pin + ';' + [...Object.values(rotation)].join(';'));
                 
-                    lastMotion = currentTimestamp;
+                        lastMotion = currentTimestamp;
+                    }
                 }
+            });
+        };
+     
+        ws.onmessage = function (evt) { 
+            var data = evt.data.split(';');
+            if (data[0] === '!SUCC') {
+                alert('Connected to user ' + data[1]);
+                window.ready = true
+            } else if (data[0] === '!ERR') {
+                alert('That pin does not exist, bringing you back to main page...');
+                window.location = '../';
+            } else if (data[0] === '!DISCON') {
+                alert('Your player left the server');
+                window.location = '../';
             }
-        });
-    };
+        };
      
-    ws.onmessage = function (evt) { 
-        var data = evt.data.split(';');
-        if (data[0] === 'SUCC') {
-            alert('Connected to user ' + data[1]);
-            window.ready = true
-        } else if (data[0] === 'ERR') {
-           alert('That pin does not exist, bringing you back to main page...');
-           window.location = '../';
-       }
-    };
-     
-    ws.onclose = function() { 
-       alert('Lost connection to server'); 
-    };
+        ws.onclose = function() { 
+            alert('Lost connection to server'); 
+        };
     }
+
+    function pewpew() {
+        if (pin) {
+            ws.send('!FIRE;' + pin);
+        }
+    };
+    
 } else {
     alert('Seems like your browser does not support websockets which is required to use this. Please try using a different browser or updating your current one');
     window.location = '../';
